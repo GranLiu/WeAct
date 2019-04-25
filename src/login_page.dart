286437f5a3c0351426636a8f6e96681cfde01115
acmilan-lyx <38382829+acmilan-lyx@users.noreply.github.com>
2019-04-25 20:57:56 +0800

    Add files via upload
    the file for login page

---
+++ src/login_page.dart
@@ -0,0 +1,116 @@
+
+//Login Page
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return new _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _email, _password;
+  bool _isObscure = true;
+  Color _eyeColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 22.0),
+              children: <Widget>[
+                SizedBox(
+                  height: kToolbarHeight,
+                ),
+                buildTitle(),
+                SizedBox(height: 70.0),
+                buildEmailTextField(),
+                SizedBox(height: 30.0),
+                buildPasswordTextField(context),
+                buildForgetPasswordText(context),
+                SizedBox(height: 60.0),
+                buildLoginButton(context),
+                SizedBox(height: 30.0),
+                buildRegisterText(context),
+              ],
+            )));
+  }
+
+  Align buildRegisterText(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Haven\'t registered yet?'),
+            GestureDetector(
+              child: Text(
+                ' Sign Up!',
+                style: TextStyle(color: Colors.green),
+              ),
+              onTap: () {
+                //TODO: RegisterPage
+                // print('go to register');
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Align buildLoginButton(BuildContext context) {
+    return Align(
+      child: SizedBox(
+        height: 45.0,
+        width: 270.0,
+        child: RaisedButton(
+          child: Text(
+            'Login',
+            style: Theme.of(context).primaryTextTheme.headline,
+          ),
+          color: Colors.black,
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              ///only the valid password can come next
+              _formKey.currentState.save();
+              //TODO the way to login
+              print('email:$_email , password:$_password');
+            }
+          },
+          shape: StadiumBorder(side: BorderSide()),
+        ),
+      ),
+    );
+  }
+
+  Padding buildForgetPasswordText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: FlatButton(
+          child: Text(
+            'Forget your password?',
+            style: TextStyle(fontSize: 14.0, color: Colors.grey),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+
+
+  //todo  else functions
+}
